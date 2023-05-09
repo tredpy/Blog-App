72ad5790418d6f618aded421f8d3b191da89afd5
tred_py <murwoofmur@gmail.com>
2023-05-09 12:16:57 +1000

    add Jest, configured jest.config.ts, add test classNames.test.ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: [
         'plugin:react/recommended',
@@ -25,7 +26,11 @@ module.exports = {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'react/jsx-filename-extension':
+            [
+                2,
+                { extensions: ['.js', '.jsx', '.tsx'] },
+            ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'warn',
@@ -38,6 +43,7 @@ module.exports = {
         'import/no-extraneous-dependencies': 'warn',
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': ['warn', { markupOnly: true }],
+        'max-len': ['error', 100, { ignoreComments: true }],
     },
     globals: {
         __IS_DEV__: true,
